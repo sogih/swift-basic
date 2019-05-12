d15b47d41475030d51b980f1317142a10b880d4c
sogih <sogih92@gmail.com>
2019-05-12 23:43:01 +0900

    newly-added

---
+++ Scene-Trans02/Scene-Trans02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scene-Trans02
+//
+//  Created by sogih on 12/05/2019.
+//  Copyright © 2019 sogih. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

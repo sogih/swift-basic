44550159119ade93fad662090bddd571b62490c3
sogih <sogih92@gmail.com>
2019-05-14 18:04:17 +0900

    newly-added

--- 04.화면전환/Scene-CustomSegue/Scene-CustomSegue/NewSegue.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class NewSegue: UIStoryboardSegue {
-    override func perform() {
-        let srcUVC = self.source
-        let destUVC = self.destination
-        UIView.transition(from: srcUVC.view, to: destUVC.view, duration: 2, options: .transitionCurlDown)
-    }
-}

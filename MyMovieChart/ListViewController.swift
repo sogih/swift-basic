52bdd654a6d8147c221f4248d6da0d984f2100d5
sogih <sogih92@gmail.com>
2019-05-27 23:36:39 +0900

    newly-added

--- MyMovieChart/ListViewController.swift
+++ MyMovieChart/ListViewController.swift
@@ -4,26 +4,11 @@ class ListViewController: UITableViewController {
     
     // 현재까지 읽어온 페이지 정보
     var page = 1
-    
-//    // 튜플 아이템으로 구성된 데이터 세트
-//    var dataset = [
-//        ("다크나이트", "영웅들에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95, "darknight.jpg"),
-//        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31, "rain.jpg"),
-//        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret.jpg")
-//    ]
+
 
     // 테이블 뷰를 구성할 리스트 데이터
     lazy var list: [MovieVO] = {
-        var datalist = [MovieVO]()  // array
-//        for (title, desc, opendate, rating, thumbnail) in self.dataset {
-//            let mvo = MovieVO()
-//            mvo.title = title
-//            mvo.description = desc
-//            mvo.opendate = opendate
-//            mvo.rating = rating
-//            mvo.thumbnail = thumbnail
-//            datalist.append(mvo)
-//        }
+        var datalist = [MovieVO]()
         return datalist
     }()
     

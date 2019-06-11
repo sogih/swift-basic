4b93544f7a4fba18cc5720e04c0b32806992e2e2
sogih <sogih92@gmail.com>
2019-06-11 10:45:52 +0900

    newly-added

--- MyMovieChart/ListViewController.swift
+++ MyMovieChart/ListViewController.swift
@@ -21,7 +21,7 @@ class ListViewController: UITableViewController {
 
 
     // 테이블 뷰를 구성할 리스트 데이터
-    lazy var list: [MovieVO] = { var datalist = [MovieVO]() retumrn datalist }()
+    lazy var list: [MovieVO] = { var datalist = [MovieVO]() return datalist }()
     
     @IBOutlet var moreBtn: UIButton!
     
